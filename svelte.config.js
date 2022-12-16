03455699c2653ad8366825bdb8991612e242cec7
Chris Adams <chris@thedreamcorporation.com>
2022-12-16 16:26:51 +0000

    Tweak SvelteKit versions and settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter(),
-    package: {
-      exports: filepath => {
-        return filepath.endsWith('.js') || filepath.endsWith('.json')
-      }
-    }
-  }
+  },
 }
 
 export default config
